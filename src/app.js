449fc6443146a43fd4a4bc69df941d418b618043
SanMasood <sanamasood.giki@gmail.com>
2020-04-23 07:00:30 +0100

    sendStatus & json: first test

--- src/app.js
+++ src/app.js
@@ -1,5 +1,15 @@
 const express = require('express');
+const strings = require ('./lib/strings.js');
 
 const app = express();
+const word = 'Hello, world!';
+
+app.get('/strings/hello/world', (req, res) => {
+    res.json({"result": `${word}`});
+
+    res.sendStatus(200);
+    
+});
+
 
 module.exports = app;
