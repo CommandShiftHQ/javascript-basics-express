3ea23f8f1d4bbacc6c0d53d6261d4a0181754b05
SanMasood <sanamasood.giki@gmail.com>
2020-04-25 09:37:02 +0100

    Customised Output

--- src/app.js
+++ src/app.js
@@ -2,9 +2,11 @@ const express = require('express');
 const strings = require ('./lib/strings.js');
 
 const app = express();
-const word = strings.sayHello();
+//const word = strings.sayHello();
 
-app.get('/strings/hello/*', (req, res) => {
+app.get('/strings/hello/:basename', (req, res) => {
+    const word = strings.sayHello(req.params.basename);
+
     res.json({"result": `${word}`});
 
     res.sendStatus(200);
