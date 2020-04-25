752c1dc7054ec08750955bb0ec43980483e818bd
SanMasood <sanamasood.giki@gmail.com>
2020-04-25 10:03:36 +0100

    Uppercase

--- src/app.js
+++ src/app.js
@@ -1,17 +1,23 @@
 const express = require('express');
-const strings = require ('./lib/strings.js');
+const strings = require('./lib/strings.js');
 
 const app = express();
-//const word = strings.sayHello();
+// const word = strings.sayHello();
 
 app.get('/strings/hello/:basename', (req, res) => {
-    const word = strings.sayHello(req.params.basename);
-    //res;
+  const word = strings.sayHello(req.params.basename);
+  // res;
 
-    res.json({"result": `${word}`}).sendStatus(200);
-
-    
+  res.json({ result: `${word}` }).sendStatus(200);
+  
 });
 
+
+app.get('/strings/upper/:basename', (req, res) => {
+  const word = strings.uppercase(req.params.basename);
+  //res.status(200);
+
+  res.json({ result: `${word}`}).sendStatus(200);
+});
 
 module.exports = app;
