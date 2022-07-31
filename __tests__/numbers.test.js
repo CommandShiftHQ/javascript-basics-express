35d5bae6435ce26c716e11af1c626973c307510b
mike-shields-dev <mr.m.p.shields@gmail.com>
2022-07-31 14:10:54 +0100

    GET /numbers/add/{number}/and/{number} errors if params are not numbers

--- __tests__/numbers.test.js
+++ __tests__/numbers.test.js
@@ -33,7 +33,7 @@ describe('/numbers', () => {
         });
     });
 
-    xit('errors if the parameters are not numbers', done => {
+    it('errors if the parameters are not numbers', done => {
       request(app)
         .get('/numbers/add/fish/and/chips')
         .then(res => {
--- src/routes/numbers.js
+++ src/routes/numbers.js
@@ -5,6 +5,9 @@ const router = Router();
 
 router.get('/add/:number1/and/:number2', (req, res) => {
   const { number1, number2 } = req.params;
+  if (Number.isNaN(Number(number1)) || Number.isNaN(Number(number2))) {
+    return res.status(400).json({ error: 'Parameters must be valid numbers.' });
+  }
   return res.status(200).json({ result: add(+number1, +number2) });
 });
 
