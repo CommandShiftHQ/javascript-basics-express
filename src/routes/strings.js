855396e3262a5e29f0ecc477f6d3eb41ae9b3a2d
mike-shields-dev <mr.m.p.shields@gmail.com>
2022-07-31 12:24:13 +0100

    GET /strings/upper/{string} returns uppercased string

--- src/routes/strings.js
+++ src/routes/strings.js
@@ -2,10 +2,17 @@ const { Router } = require('express');
 
 const router = Router();
 
-router.get('/hello/:string', (req, res) => {
-  return res.status(200).json({
-    result: `Hello, ${req.params.string}!`,
-  });
+router
+  .get('/hello/:string', (req, res) => {
+    return res.status(200).json({
+      result: `Hello, ${req.params.string}!`,
+    });
+  })
+  .get('/upper/:string', (req, res) => {
+    return res.status(200).json({
+      result: req.params.string.toUpperCase(),
+    });
+  })
 });
 
 module.exports = router;
