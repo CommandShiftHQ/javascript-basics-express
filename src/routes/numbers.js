f1a8150d1e8b444f73c256f03f6eea7362e2fabc
mike-shields-dev <mr.m.p.shields@gmail.com>
2022-07-31 13:37:59 +0100

    GET /numbers/add/{number1}/and/{number2} adds 2 and 1

--- src/routes/numbers.js
+++ src/routes/numbers.js
@@ -2,4 +2,10 @@ const { Router } = require('express');
 
 const router = Router();
 
+router.get('/add/:number1/and/:number2', (req, res) => {
+  const { number1, number2 } = req.params;
+  const result = Number(number1) + Number(number2);
+  res.json({ result });
+});
+
 module.exports = router;
