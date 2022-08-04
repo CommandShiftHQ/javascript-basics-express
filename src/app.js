f56041cf18963b5d53a5c7e1fa93176b085aba16
Suzannah Barnes <smgarland@hotmail.co.uk>
2022-08-04 21:16:09 +0100

    post working now

--- src/app.js
+++ src/app.js
@@ -1,5 +1,82 @@
 const express = require('express');
 
+const {
+  sayHello,
+  uppercase,
+  lowercase,
+  firstCharacter,
+  firstCharacters,
+} = require('./lib/strings');
+
+const {
+  add,
+  subtract,
+  multiply,
+  divide,
+  power,
+  round,
+  roundUp,
+  roundDown,
+  absolute,
+  quotient,
+  remainder,
+} = require('./lib/numbers');
+
 const app = express();
+
+app.use(express.json());
+
+app.get('/strings/hello/:string', (req, res) => {
+  res.status(200).json({ result: sayHello(req.params.string) });
+});
+
+app.get('/strings/upper/:string', (req, res) => {
+  res.status(200).json({ result: uppercase(req.params.string) });
+});
+
+app.get('/strings/lower/:string', (req, res) => {
+  res.status(200).json({ result: lowercase(req.params.string) });
+});
+
+app.get('/strings/first-characters/:string', (req, res) => {
+  const { string } = req.params;
+  const { length } = req.query;
+  let result;
+  if (length) {
+    result = firstCharacters(string, length);
+  } else {
+    result = firstCharacter(string);
+  }
+  res.status(200).json({ result });
+});
+
+/* Numbers */
+
+app.get('/numbers/add/:a/and/:b', (req, res) => {
+  const a = parseInt(req.params.a, 10);
+  const b = parseInt(req.params.b, 10);
+
+  return Number.isNaN(a) || Number.isNaN(b)
+    ? res.sendStatus(400)
+    : res.status(200).json({ result: add(a, b) });
+});
+
+app.get('/numbers/subtract/:a/from/:b', (req, res) => {
+  const a = parseInt(req.params.a);
+  const b = parseInt(req.params.b);
+
+  if (a === 'NaN' && b === 'NaN') {
+    res.status(400).send({ error: 'Parameters must be valid numbers.' });
+  } else {
+    res.status(200).send({ result: subtract(b, a) });
+  }
+});
+
+app.post('/numbers/multiply', (req, res) => {
+  const { a } = req.body;
+  const { b } = req.body;
+  res.status(200).send({ result: multiply(a, b) });
+  /* res.status(400).send({ error: 'Parameters "a" and "b" are required.' }); */
+});
 
 module.exports = app;
